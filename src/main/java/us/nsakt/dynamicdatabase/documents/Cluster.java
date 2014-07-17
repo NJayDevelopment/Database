3d19b64ee0e4000cc92afa2fbee23a431398f9f8
Austin Mayes <bookofnathan@gmail.com>
2014-07-16 21:40:48 -0500

    Add visibility and populate cluster class

--- src/main/java/us/nsakt/dynamicdatabase/documents/Cluster.java
+++ src/main/java/us/nsakt/dynamicdatabase/documents/Cluster.java
@@ -1,8 +1,53 @@
 package us.nsakt.dynamicdatabase.documents;
 
+import org.mongodb.morphia.annotations.Entity;
+import us.nsakt.dynamicdatabase.util.Visibility;
+
 /**
  * Class to represent a "cluster" of documents.
- * Usually represents a datastore.
  */
+@Entity("clusters")
 public class Cluster extends Document {
+    private String name;
+    private Visibility visibility;
+
+    /**
+     * Get the cluster's name
+     * @return
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Set the cluster's name
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Get the cluster's visibility
+     * @return
+     */
+    public Visibility getVisibility() {
+        return visibility;
+    }
+
+    /**
+     * Set the cluster's visibility
+     * @param visibility
+     */
+    public void setVisibility(Visibility visibility) {
+        this.visibility = visibility;
+    }
+
+    @Override
+    public String toString() {
+        return "Cluster{" +
+                "name='" + name + '\'' +
+                ", visibility=" + visibility +
+                '}';
+    }
 }
---
+++ src/main/java/us/nsakt/dynamicdatabase/util/Visibility.java
@@ -0,0 +1,21 @@
+package us.nsakt.dynamicdatabase.util;
+
+/**
+ * Class to represent the visibility of something.
+ */
+public enum Visibility {
+    PUBLIC("public"),
+    PRIVATE("private"),
+    STAFF_ONLY("staff-only");
+
+
+    public String dbName;
+
+    /**
+     * Default constructor
+     * @param dbName name for database reference
+     */
+    Visibility(String dbName) {
+        this.dbName = dbName;
+    }
+}
