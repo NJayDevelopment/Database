e2295a0b070fe6de795e10d41f350f53de72b945
Austin Mayes <bookofnathan@gmail.com>
2014-07-21 14:45:00 -0500

    Add display name to Visibility

--- src/main/java/us/nsakt/dynamicdatabase/util/Visibility.java
+++ src/main/java/us/nsakt/dynamicdatabase/util/Visibility.java
@@ -4,19 +4,21 @@ package us.nsakt.dynamicdatabase.util;
  * Class to represent the visibility of something.
  */
 public enum Visibility {
-    PUBLIC("public"),
-    PRIVATE("private"),
-    STAFF_ONLY("staff-only");
+    PUBLIC("public", "Public"),
+    PRIVATE("private", "Private"),
+    STAFF_ONLY("staff-only", "Staff Only");
 
 
     public String dbName;
+    public String displayName;
 
     /**
      * Default constructor
      *
      * @param dbName name for database reference
      */
-    Visibility(String dbName) {
+    Visibility(String dbName, String displayName) {
         this.dbName = dbName;
+        this.displayName = displayName;
     }
 }
